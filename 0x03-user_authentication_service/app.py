62a1894fee3d81e8dda5b888622e38ce8a942294
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-12 01:27:48 +0300

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -38,5 +38,30 @@ def register_user() -> str:
           jsonify({"message": "email already registered"}), 400)
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """POST /sessions
+    JSON body:
+      - email: the user's email
+      - password: the user's password
+    Return:
+      - a JSON response with the new user's email
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not auth.valid_login(email, password):
+        abort(401)
+
+    session_id = auth.create_session(email)
+    if not session_id:
+        abort(401)
+
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+
+    return response
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
