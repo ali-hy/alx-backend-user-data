88c129e22f3bb7cf843b072b008231605d3aa97d
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-13 02:01:03 +0300

    attempt to finish fast

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -99,5 +99,39 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """POST /reset_password
+    JSON body:
+      - email: the user's email
+    Return:
+      - a JSON response with the reset token
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = auth.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """PUT /reset_password
+    JSON body:
+      - email: the user's email
+    Return:
+      - a JSON response with the reset token
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        auth.update_password(reset_token, new_password)
+    except Exception:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
