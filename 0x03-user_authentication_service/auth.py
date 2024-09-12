cb367ac08cad1c17eaa17d2b02e0df699af92e5e
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-13 01:16:36 +0300

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,7 +43,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode(), user.hashed_password)
+            return bcrypt.checkpw(password.encode(), user.hashed_password.encode())
         except NoResultFound:
             return False
 
@@ -55,5 +55,16 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """Get a user from a session ID
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
