d904fdd68e3af00079c055b90efe15d8155d2c4e
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-13 01:19:51 +0300

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,6 +62,23 @@ def login() -> str:
 
     return response
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """DELETE /sessions
+    JSON body:
+      - session_id: the session_id
+    Return:
+      - an empty JSON response
+    """
+    session_id = request.cookies.get('session_id')
+    user = auth.get_user_from_session_id(session_id)
+
+    if not user:
+        abort(403)
+
+    auth.destroy_session(user.id)
+
+    return jsonify({}), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
