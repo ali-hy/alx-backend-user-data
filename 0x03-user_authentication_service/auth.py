c98f75403d29dde382387b437b84da2bdcd40deb
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-13 01:44:48 +0300

    logout fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,7 +44,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode(),
-                                  user.hashed_password.encode())
+                                  user.hashed_password)
         except NoResultFound:
             return False
 
